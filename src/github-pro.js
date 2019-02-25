44f960709c132ab9be50955ab730dd55e8e07e5f
Mulia Arifandi Nasution <mul14.net@gmail.com>
2019-02-25 20:24:49 +0700

    jQuery is not required
    Signed-off-by: Mulia Arifandi Nasution <mul14.net@gmail.com>

--- src/github-pro.js
+++ src/github-pro.js
@@ -1,7 +1,1 @@
-// Use jQuery to add a class to the html
-
-$(document).ready(function() {
-  $(
-    "#js-pjax-container > div > div.h-card.col-3.float-left.pr-3 > div.border-top.border-gray-light.py-3.js-pro-badge.d-none"
-  ).removeClass("d-none");
-});
+document.querySelector('.js-pro-badge').classList.remove('d-none')
